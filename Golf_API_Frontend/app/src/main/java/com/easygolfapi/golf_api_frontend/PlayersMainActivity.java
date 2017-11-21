550289bff41bb553c3a57b1692451573bb8b136e
Adam  Sunderman <sunderad@oregonstate.edu>
2017-11-21 10:55:49 -0800

    Added recycler and player layout.

--- Golf_API_Frontend/app/src/main/java/com/easygolfapi/golf_api_frontend/PlayersMainActivity.java
+++ Golf_API_Frontend/app/src/main/java/com/easygolfapi/golf_api_frontend/PlayersMainActivity.java
@@ -2,6 +2,9 @@ package com.easygolfapi.golf_api_frontend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class PlayersMainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class PlayersMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players_main);
+
+
     }
 }
