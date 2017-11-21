79414b9b32bd52b56c80beb3eef6732aa06c7e2e
Adam  Sunderman <sunderad@oregonstate.edu>
2017-11-21 09:00:35 -0800

    Added Home buttons and two activities, player main + sign up.

--- Golf_API_Frontend/app/src/main/java/com/easygolfapi/golf_api_frontend/MainActivity.java
+++ Golf_API_Frontend/app/src/main/java/com/easygolfapi/golf_api_frontend/MainActivity.java
@@ -1,9 +1,10 @@
 package com.easygolfapi.golf_api_frontend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.easygolfapi.golf_api_frontend.R;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 =  (Button) findViewById(R.id.home_browse_button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlayersMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2 =  (Button) findViewById(R.id.home_signup_button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MainActivity.this, PlayersSignUpActivity.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
 
---
+++ Golf_API_Frontend/app/src/main/java/com/easygolfapi/golf_api_frontend/PlayersMainActivity.java
@@ -0,0 +1,13 @@
+package com.easygolfapi.golf_api_frontend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayersMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_players_main);
+    }
+}
