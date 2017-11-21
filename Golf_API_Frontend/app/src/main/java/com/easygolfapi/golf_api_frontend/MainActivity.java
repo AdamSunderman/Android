d5c9d919edbddc82d67433097b6611a0d434e29a
Adam  Sunderman <sunderad@oregonstate.edu>
2017-11-21 06:38:30 -0800

    Start

---
+++ Golf_API_Frontend/app/src/main/java/com/easygolfapi/golf_api_frontend/MainActivity.java
@@ -0,0 +1,18 @@
+package com.easygolfapi.golf_api_frontend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.easygolfapi.golf_api_frontend.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
