8f59f6ddd44ca49a3b87717cf83ef6a489c10e24
Adam  Sunderman <sunderad@oregonstate.edu>
2017-11-23 12:59:50 -0800

    new App

--- Golf_API_Frontend/app/src/main/java/com/easygolfapi/golf_api_frontend/MainActivity.java
+++ Golf_API_Frontend/app/src/main/java/com/easygolfapi/golf_api_frontend/MainActivity.java
@@ -1,20 +1,30 @@
 package com.easygolfapi.golf_api_frontend;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.Context;
+
 
 import com.android.volley.RequestQueue;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int INTERNET_PERMISSION_RESULT = 17;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RequestQueue queue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.INTERNET}, INTERNET_PERMISSION_RESULT);
+        }
 
         Button button1 =  (Button) findViewById(R.id.home_browse_button);
         button1.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == INTERNET_PERMISSION_RESULT){
+            if(grantResults.length > 0){
+            }
+        }
+    }
+
 }
 
 
--- Golf_API_Frontend/app/src/main/java/com/easygolfapi/golf_api_frontend/PlayersMainActivity.java
+++ Golf_API_Frontend/app/src/main/java/com/easygolfapi/golf_api_frontend/PlayersMainActivity.java
@@ -6,6 +6,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class PlayersMainActivity extends AppCompatActivity {
 
     @Override
@@ -13,6 +23,29 @@ public class PlayersMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players_main);
 
+        final TextView mTest = (TextView) findViewById(R.id.testTextView);
 
+        String getAllPlayersUrl = "https://easygolfapi.appspot.com/players";
+
+        RequestQueue queue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+
+        StringRequest mStringRequest = new StringRequest(
+                Request.Method.GET,
+                getAllPlayersUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        mTest.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mTest.setText(error.getMessage());
+                    }
+                }
+        );
+
+        MySingleton.getInstance(this).addToRequestQueue(mStringRequest);
     }
 }
